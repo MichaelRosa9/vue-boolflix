3b72c0ad2d4beebb976530141256f1da8d1b4115
MichaelRosa9 <michaelrosa9@gmail.com>
2021-05-26 17:34:33 +0200

    input

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <div class="logo"></div>
+      <Nav v-on:searchText="searchShow($event)" />
+    </header>
+
+    <main>
+
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Nav from './components/Nav.vue'
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav
+  },
+  data(){
+    return{
+      apiURL: 'https://api.themoviedb.org/3/search/movie',
+      apiKey: 'be3fa3bd2b20925e1d6c0b3adfcb12d6',
+      query: 'back to the future',
+      lang: 'it-IT',
+      inputTextNav: '',
+      
+    }
+  },
+  created(){
+    axios.get(this.apiURL,{
+      params: {
+        api_key: this.apiKey,
+        query: this.query,
+        language: this.lang
+      }
+    }).then(response => {
+    console.log(response.data);
+    }).catch(err => {
+      console.log(err);
+    })
+  },
+  methods: {
+    searchShow($event){
+      this.inputTextNav = $event;
+      console.log(this.inputTextNav)
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import '@/styles/general.scss';
+@import '@/styles/utilities.scss';
+
+header {
+  background-color: red;
+}
+main {
+  background-color: black;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,52 @@
+<template>
+ <nav>
+   <input type="text"
+    v-model="inputText"
+    @keyup.enter="searchText(inputText)"
+   >
+   <button
+    @click="searchText(inputText)"
+   >search</button>
+   <select name="language" id="">
+     <option value="">Select Language</option>
+   </select>
+ </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  props: {
+   
+  },
+  data(){
+    return{
+      inputText:''
+    }
+  },
+  methods: {
+    searchText(){
+      this.$emit('searchText', this.inputText);
+      this.inputText = '';
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+nav {
+
+  input, button, select {
+    height: 50%;
+    margin-right: 20px;
+    padding: 5px;
+    border: none;
+    border-radius: 4px;
+  }
+  input {
+    outline: none;
+  }
+}
+
+</style>
