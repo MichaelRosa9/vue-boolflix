e7005ae3dca49731f0f461994bebe356740c1ad1
MichaelRosa9 <michaelrosa9@gmail.com>
2021-05-26 18:49:55 +0200

    axios

--- src/App.vue
+++ src/App.vue
@@ -6,54 +6,64 @@
     </header>
 
     <main>
+      <div class="container">
+        <Thumbnail class="thumbnail" 
+          v-for="show in shows" :key="show.id"
+          :show="show"
+         />
 
+      </div>
     </main>
   </div>
 </template>
 
 <script>
-import Nav from './components/Nav.vue'
 import axios from 'axios'
+import Nav from './components/Nav.vue';
+import Thumbnail from './components/Thumbnail.vue';
 export default {
   name: 'App',
   components: {
-    Nav
+    Nav,
+    Thumbnail
   },
   data(){
     return{
       apiURL: 'https://api.themoviedb.org/3/search/movie',
       apiKey: 'be3fa3bd2b20925e1d6c0b3adfcb12d6',
-      query: 'back to the future',
+      query: '',
       lang: 'it-IT',
       inputTextNav: '',
-      
+      shows: [],
+    
     }
   },
   created(){
-    axios.get(this.apiURL,{
-      params: {
-        api_key: this.apiKey,
-        query: this.query,
-        language: this.lang
-      }
-    }).then(response => {
-    console.log(response.data);
-    }).catch(err => {
-      console.log(err);
-    })
+    
   },
   methods: {
     searchShow($event){
       this.inputTextNav = $event;
-      console.log(this.inputTextNav)
+      axios.get(this.apiURL,{
+      params: {
+        api_key: this.apiKey,
+        query: $event,
+        language: this.lang
+      }
+    }).then(response => {
+      this.shows = response.data.results;
+      
+    }).catch(err => {
+      console.log(err);
+    })
     }
   }
 }
 </script>
 
 <style lang="scss">
-@import '@/styles/general.scss';
-@import '@/styles/utilities.scss';
+@import './styles/general.scss';
+@import './styles/utilities.scss';
 
 header {
   background-color: red;
--- src/components/Thumbnail.vue
+++ src/components/Thumbnail.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
-
+    <h3>
+      {{ show.title }}
+    </h3>
+    <h4>
+      {{ show.originaltitle }}
+    </h4>
+    <h5>
+      Lang: {{ show.original_languag }}
+    </h5>
+    <h6>
+      Rated: {{ show.vote_average }}
+    </h6>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Thumbnail',
+  props:{
+    show: Object
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 
+  div {
+    height: 250px;
+    width: 350px;
+    background-color: red;
+    margin-top: 40px;
+
+    h3 {
+      text-align: center;
+    }
+  }
 </style>
